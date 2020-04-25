74008f4ed6be72ac04dd41608f7b7b9f041c5600
n-hata91 <htnoms5a@gmail.com>
2020-04-25 11:34:07 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '831905790e995a8e3dc449f40a2bf12ca46d3687a2ee55492adba1481994514c95596f73c1b77f305ffccf0e55df1c6f5f3c3f0909ee2712e012525ba75ab7eb'
